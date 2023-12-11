ddbe28b50c39b2571b0fd91e24d36028e5193447
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2023-12-11 13:59:39 +0530

    Time: 6 ms (92.41%) | Memory: 12.7 MB (72.53%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,19 +1,18 @@
+#include <vector>
+
 class Solution {
 public:
-    int findSpecialInteger(vector<int>& arr) {
-        map<int, int> mp;
-        for(int i=0; i<arr.size(); i++){
-            mp[arr[i]]++;
+    int findSpecialInteger(std::vector<int>& arr) {
+        int quarter = arr.size() / 4;
+
+        
+        for (int i = 0; i < arr.size() - quarter; i++) {
+            if (arr[i] == arr[i + quarter]) {
+                return arr[i];
+            }
         }
-        int max =0;
-        int update;
-        for(auto i: mp){
-           if(i.second > max){
-               max = i.second;
-               update = i.first;
-               //update = i.first;
-           }
-        }
-        return update ;
+
+        
+        return -1;
     }
 };
