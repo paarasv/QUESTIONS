e39daf7ecdd2df5135fb4c4c5986d2a007fc62af
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2024-03-14 23:21:46 +0530

    Time: 1954 ms (5.02%) | Memory: 31.1 MB (85.39%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,29 @@
+//This will run
+//Although time complexity is (n^2)
+//some cases are handeled smartly 
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int res=0;
+        int n=size(nums);
+        for(int s=0;s<n;s++){
+            int sum=0;
+            int flag=0;
+            for(int e=s;e<n;e++){
+                sum+=nums[e];
+                if(sum==goal){
+                    flag=1;
+                    res++;
+                    e++;
+                    while(e<n && nums[e]==0){
+                        e++;
+                        res++;
+                    }                   
+                    break;
+                }
+            }
+            if(flag==0 && goal!=0)break;
+        }
+        return res;
+    }
+};
