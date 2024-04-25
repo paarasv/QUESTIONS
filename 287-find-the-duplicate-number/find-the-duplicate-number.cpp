3300d55f6849029bbeed3725f80bae030c8e9fba
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2024-04-25 21:19:29 +0530

    Time: 179 ms (9.63%) | Memory: 104.3 MB (6.52%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int findDuplicate(std::vector<int>& nums) {
-        int tortoise = nums[0];
-        int hare = nums[0];
-
-        // Phase 1: Detect if there's a cycle
-        do {
-            tortoise = nums[tortoise];
-            hare = nums[nums[hare]];
-        } while (tortoise != hare);
-
-        // Phase 2: Find the entrance to the cycle
-        tortoise = nums[0];
-        while (tortoise != hare) {
-            tortoise = nums[tortoise];
-            hare = nums[hare];
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
         }
-
-        return hare; // or tortoise, as they both point to the entrance of the cycle
+        for(auto x:mp){
+            if(x.second>1){
+                return x.first;
+            }
+        }
+        return -1;
     }
 };
