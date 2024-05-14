85a03cc972ffb31a8f6fb0962f3464e313471edc
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2024-05-14 22:59:43 +0530

    Time: 7 ms (99.93%) | Memory: 30.5 MB (90.26%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -4,20 +4,24 @@
 class Solution {
 public:
     int minSubArrayLen(int target, std::vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+        int mini = INT_MAX;
         int n = nums.size();
-        int mini = INT_MAX; // Initialize the minimum length of subarray
-        int sum = 0; // Initialize the sum of the current window
-        int left = 0; // Initialize the left pointer
+        long long sum = 0;
         
-        for (int right = 0; right < n; right++) { // Iterate over the array
-            sum += nums[right]; // Add the current element to the sum
+        while (j < n) {
+            sum += nums[j];
             
-            while (sum >= target) { // Shrink the window as long as the sum is greater than or equal to the target
-                mini = std::min(mini, right - left + 1); // Update the minimum length of subarray
-                sum -= nums[left++]; // Remove the leftmost element from the window
+            while (sum >= target) {
+                mini = std::min(mini, j - i + 1);
+                sum -= nums[i];
+                i++;
             }
+            
+            j++;
         }
         
-        return (mini == INT_MAX) ? 0 : mini; // Return the minimum length of subarray
+        return (mini == INT_MAX) ? 0 : mini;
     }
 };
