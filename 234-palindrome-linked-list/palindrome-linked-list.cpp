737d242b187a3b73f47b9f6b3874e3a731aa4825
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2024-06-11 02:47:26 +0530

    Time: 163 ms (69.57%) | Memory: 116.5 MB (81.98%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,17 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    // Function to reverse a linked list
+    ListNode* reverse(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        while (curr != nullptr) {
+            ListNode* next = curr->next; // Save the next node
+            curr->next = prev;           // Reverse the current node's pointer
+            prev = curr;                 // Move prev to the current node
+            curr = next;                 // Move to the next node
+        }
+        return prev;                     // Return the new head (the previous tail)
+    }
+
+    // Function to check if the linked list is a palindrome
     bool isPalindrome(ListNode* head) {
-        vector<int> listVals;
-        while (head) {
-            listVals.push_back(head->val);
-            head = head->next;
+        if (head == nullptr) return true; // Edge case for empty list
+
+        // Step 1: Find the middle of the linked list
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        
-        int left = 0, right = listVals.size() - 1;
-        while (left < right && listVals[left] == listVals[right]) {
-            left++;
-            right--;
+
+        // Step 2: Reverse the second half of the linked list
+        ListNode* rev = reverse(slow);
+
+        // Step 3: Compare the first half and the reversed second half
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = rev;
+        while (secondHalf != nullptr) {
+            if (firstHalf->val != secondHalf->val) {
+                return false;
+            }
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
         }
-        return left >= right;
+        return true;
     }
 };
