609e7ab61d890cc0056de82685e4f9f584630581
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2024-08-23 22:29:12 +0530

    Time: 15 ms (40.47%) | Memory: 22.2 MB (72.77%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -1,25 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        // Define a helper function to calculate the diameter recursively
-        int res = 0;
-        diameter(root, res);
-        return res;
+    int diameter(TreeNode* root , int& ans){
+       if (root==nullptr){
+        return 0; 
+       }
+       int left = diameter(root->left , ans);
+       int right = diameter(root->right , ans);
+       ans = max(ans , left+right);
+       return max(left,right)+1;
     }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans =0;
+        diameter(root , ans);
+        return ans;
 
-private:
-    int diameter(TreeNode* curr, int& res){
-        // Base case: if the current node is null, return 0
-        if (!curr) return 0;
-        
-        // Recursively calculate the diameter of left and right subtrees
-        int left = diameter(curr->left, res);
-        int right = diameter(curr->right, res);
-
-        // Update the maximum diameter encountered so far
-        res = std::max(res, left + right);
-        
-        // Return the depth of the current node
-        return std::max(left, right) + 1;
     }
 };
