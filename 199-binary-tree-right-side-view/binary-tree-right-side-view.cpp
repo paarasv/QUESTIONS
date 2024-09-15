7c572433177b4fdf3916b1729f0b7a775517e64d
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2024-09-16 01:49:17 +0530

    Time: 4 ms (31.76%) | Memory: 14.5 MB (36.00%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -6,35 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        queue<TreeNode*> q;
-        if (root == nullptr) {
+        vector<int>ans;
+        if(root==nullptr){
             return ans;
         }
+        queue<TreeNode*>q;
         q.push(root);
-        while (!q.empty()) {
+        while(!q.empty()){
             int k = q.size();
             vector<int>level;
-            for (int i = 0; i < k; i++) {
-                TreeNode* temp = q.front();
-                q.pop();
-                level.push_back(temp->val);
-                if(temp->right!=nullptr){
-                    q.push(temp->right);
-                }
-                if(temp->left!=nullptr){
-                    q.push(temp->left);
-                }
+            for(int i=0; i<k ; i++){
+            TreeNode*temp = q.front();
+            q.pop();
+            level.push_back(temp->val);
+            if(temp->right!=nullptr){
+                q.push(temp->right);
+            }
+            if(temp->left!=nullptr){
+                q.push(temp->left);
+            }
             }
             ans.push_back(level[0]);
         }
         return ans;
+
     }
 };
