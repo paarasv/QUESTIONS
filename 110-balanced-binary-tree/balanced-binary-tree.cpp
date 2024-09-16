7c9f24d63c5b86882bc7d84ef10d1b23f797fa07
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2024-09-17 05:26:08 +0530

    Time: 4 ms (93.55%) | Memory: 21.7 MB (90.24%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,33 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (root == NULL) {
+    int balncedornot(TreeNode* root , bool & balanced){
+        //balanced = true;
+        if(root==nullptr){
             return 0;
         }
-        int l = height(root->left);
-        int r = height(root->right);
-        int maxii = 1 + max(l, r);
-        return maxii;
+
+        int lh = balncedornot(root->left , balanced);
+        int rh = balncedornot(root->right , balanced);
+        if(abs(lh-rh)>1){
+            balanced = false;
+        }
+
+        return max(lh,rh)+1;
     }
-
     bool isBalanced(TreeNode* root) {
-        if (root == NULL) {
-            return true;
-        }
-
-        int left = height(root->left);
-        int right = height(root->right);
-        int diff = abs(left - right);
-        bool ans = diff <= 1;
-
-        // Recursion to calculate left and right
-        bool leftans = isBalanced(root->left);
-        bool rightans = isBalanced(root->right);
-
-        if (ans && leftans && rightans) {
-            return true;
-        } else {
-            return false;
-        }
+        bool balanced = true;
+        balncedornot(root, balanced);
+        return balanced;
     }
 };
