f50d693ffac42b51807db8bcf83867624576caa9
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2024-09-23 19:46:51 +0530

    Time: 85 ms (11.17%) | Memory: 48.4 MB (24.70%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<int> result; // Result vector to store duplicate numbers
-        for (int i = 0; i < nums.size(); ++i) {
-            int index = abs(nums[i]) - 1; // Get the index corresponding to the number
-            if (nums[index] < 0) { // If the number at index is negative, it means it's seen before
-                result.push_back(index + 1); // Add the duplicate number to the result
-            } else {
-                nums[index] = -nums[index]; // Mark the number as seen by negating it
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0; i<nums.size(); i++){
+           
+              mp[nums[i]]++;
+            
+        }
+        for(int i=0 ; i<nums.size(); i++){
+            if(mp[nums[i]]==2){
+                ans.push_back(nums[i]);
+                mp[nums[i]]=0;
             }
         }
-        return result; // Return the vector containing duplicate numbers
+
+        return ans;
     }
 };
