320c8aff4e3af2690b5857fe153de20f66e72b58
PARAS VERMA <126498196+paarasv@users.noreply.github.com>
2024-11-12 16:00:20 +0530

    Time: 4 ms (51.46%) | Memory: 17.5 MB (57.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,45 +1,25 @@
-#include <vector>
-#include <map>
-#include <queue>
-#include <stack>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        map<int, int> mp;
-        stack<int> st;
-        vector<int> vt;
-
-        if (nums.size() == 1) {
-            return nums;
-        }
-
-        int cnt = 0;
-        for (int i = 0; i < nums.size(); i++) {
+        unordered_map<int,int>mp;
+        for(int i=0; i<nums.size(); i++){
             mp[nums[i]]++;
         }
 
-        for (auto x : mp) {
-            pq.push(x.second);
-            if (pq.size() > k) {
+        priority_queue < pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>>pq;
+        for(auto x: mp){
+            pq.push(make_pair(x.second , x.first));
+            if(pq.size()>k){
                 pq.pop();
             }
         }
+     vector<int>result;
+     while(!pq.empty()){
+        result.push_back(pq.top().second);
+        pq.pop();
+     }
+return result;
 
-        for (auto x : mp) {
-            if (x.second >= pq.top()) {
-                st.push(x.first);
-            }
-        }
-
-        while (!st.empty()) {
-            vt.push_back(st.top());
-            st.pop();
-        }
-
-        return vt;
+        
     }
 };
